e3c8f1f52c89d786b227b8923836294b585ebb6b
daniel <daniel.pinedamo@amigo.edu.co>
2023-08-18 15:31:29 -0500

    archivos para deploy

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:faztweb/react-rick-and-morty.git main:gh-pages
+git push -f git@github.com:DanielPinedaM/react-rick-and-morty-api.git main:gh-pages
 
 cd -
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://faztweb.github.io/react-rick-and-morty/",
+  base: "https://DanielPinedaM.github.io/react-rick-and-morty-api/",
   plugins: [react(), eslint()],
 });
